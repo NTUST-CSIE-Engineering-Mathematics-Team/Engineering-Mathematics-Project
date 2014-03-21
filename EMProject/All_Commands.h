3bb0271f739f1c641f0caa0a5871d8bd042c1189
Maeglin Liao <maeglin89273@gmail.com>
2014-03-21 13:09:38 +0800

    #save point 3

--- EMProject/All_Commands.h
+++ EMProject/All_Commands.h
@@ -1,11 +1,10 @@
 #pragma once
 
 #include "PrintCommand.h"
-#include "CreateMatrixCommand.h"
-#include "CreateVectorCommand.h"
-#include "CreateScalarCommand.h"
 #include "SettingsCommand.h"
 #include "DeleteCommand.h"
 #include "ShowStorageCommand.h"
+#include "CreateMathObjectCommand.h"
 #include "JudgeCommand.h"
+
 #include "CommandFactory.h"
